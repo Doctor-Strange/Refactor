162b0d7c93bb1238021e184117beb3b32d121788
doctor-stranger <sepehrfake@gmail.com>
2021-05-03 16:10:00 +0430

    cypress complete regiater

--- cypress/integration/complete_register/complete_register.spec.js
+++ cypress/integration/complete_register/complete_register.spec.js
@@ -9,10 +9,8 @@ let user_info = 0;
 let cell_phone = "09380000000";
 
 describe("بررسی صفحه تکمیل ثبت نام", () => {
-  beforeEach(() => {
+  it("لاگین کاربر و تکمیل ثبت نام", () => {
     cy.visit(home);
-  });
-  it("بررسی دسترسی های صفحه", () => {
     log_me_in(cell_phone).then(() => {
       cy.request({
         method: "POST",
@@ -24,8 +22,33 @@ describe("بررسی صفحه تکمیل ثبت نام", () => {
         },
       }).then((data) => {
         user_info = data.body;
-        cy.url().should("contain", "/complete-register");
+        cy.url()
+          .should("contain", "/complete-register")
+          .get(".complete_register_form [name=first_name]")
+          .type("Cypress")
+          .get(".complete_register_form [name=last_name]")
+          .type("io")
+          .get(".company_part")
+          .click()
+          .get(".company_part [name=company_name]")
+          .type("Cypress_co")
+          .get(".Blue_BTN.local_BTN.HEAP_CompleteRegister_Btn_Submit")
+          .click()
+          .get(".company_part span")
+          .click()
+          .should("not.exist")
+          .get(".Blue_BTN.local_BTN.HEAP_CompleteRegister_Btn_Submit")
+          .click()
+          .get(".Error_message_text")
+          .should("exist")
+          .get(".check_box_container label")
+          .click()
+          .get(".Blue_BTN.local_BTN.HEAP_CompleteRegister_Btn_Submit")
+          .click();
       });
     });
+  });
+  it("ثبت خودرو جدید", () => {
+    cy.visit(`${home}/add-car`);
   });
 });
