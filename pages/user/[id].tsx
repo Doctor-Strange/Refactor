992dec3b56070fd80590faf7e126ca455fe42177
seper_saderi <saderi.sajad@gmail.com>
2021-08-17 11:23:28 +0430

    clean up

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -2,7 +2,6 @@ import dynamic from "next/dynamic";
 
 const Layout = dynamic(() => import("../../src/Layout"));
 // import Layout from "../../src/Layout";
-// import language from "../../public/languages/fa/user.json";
 import language from "../../public/languages/en/user.json";
 // import { logPageView } from "../../utils/analytics";
 
